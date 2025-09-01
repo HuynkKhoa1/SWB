f6744f3d9a21c9387a0f3c41428bed8a1c0b71c6
Khoa <86233699+HuynkKhoa1@users.noreply.github.com>
2025-09-01 04:28:16 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5a52d7eb84373963bd2ef96cf071f52b.lua"))()
+repeat task.wait() until game:IsLoaded()
+pcall(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5a52d7eb84373963bd2ef96cf071f52b.lua"))()
+end)
